789ee1d520f2eec97049e6d89f2c9c803577a51d
Emre Karayaman <karayaman.emre@outlook.com>
2023-02-20 13:18:03 +0300

    controller created and seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greetings.create([
+  { message: "Hello" },
+  { message: "Hi" },
+  { message: "Hey there" },
+  { message: "What's up?" },
+  { message: "Greetings" }
+])
